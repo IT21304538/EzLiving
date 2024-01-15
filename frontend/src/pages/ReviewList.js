937c3f9a05ef467fcdd3dcac22ad2adf3993375d
Hirudaka Kumarasinghe <it21304538@my.sliit.lk>
2024-01-15 22:42:21 +0530

    Update ReviewList.js

--- frontend/src/pages/ReviewList.js
+++ frontend/src/pages/ReviewList.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Searchbar from "../components/Searchbar";
+
 function ReviewList() {
   const [reviewList, setReviewList] = useState([]);
   const { user } = useAuthContext();
